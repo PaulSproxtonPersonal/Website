0a4fd8c4e1a259def0dcd16274ceb160168dddf3
Paul Sproxton <sproxton.paul@gmail.com>
2025-03-12 14:17:46 -0400

    Add the Decking section to the Handyman page.

--- frontend/src/pages/Handyman.jsx
+++ frontend/src/pages/Handyman.jsx
@@ -201,6 +201,185 @@ function Handyman() {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="container container-lg projects-grid">
+                            <div className="project">
+                                <h3>Laminate Flooring installation</h3>
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Laminate\After1.jpg">
+                                            <img src="..\..\images\Flooring\Laminate\After1.jpg" alt="Laminate" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container container-lg projects-grid">
+                            <div className="project">
+                                <h3>Luxury Vinyl Plank installation</h3>
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Vinyl\House1\Image1.jpg">
+                                            <img src="..\..\images\Flooring\Vinyl\House1\Image1.jpg" alt="Vinyl 1" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Vinyl\House1\Image2.jpg">
+                                            <img src="..\..\images\Flooring\Vinyl\House1\Image2.jpg" alt="Vinyl 2" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Vinyl\House1\Image3.jpg">
+                                            <img src="..\..\images\Flooring\Vinyl\House1\Image3.jpg" alt="Vinyl 3" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Vinyl\House1\Image4.jpg">
+                                            <img src="..\..\images\Flooring\Vinyl\House1\Image4.jpg" alt="Vinyl 4" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Flooring\Vinyl\House1\Image5.jpg">
+                                            <img src="..\..\images\Flooring\Vinyl\House1\Image5.jpg" alt="Vinyl 5" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container container-lg projects-grid">
+                            <div className="project">
+                                <h3>Deck Resurfacing and Leveling</h3>
+                                <p>Before</p>
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before1.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before1.jpg" alt="Before 1" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before2.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before2.jpg" alt="Before 2" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before3.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before3.jpg" alt="Before 3" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before4.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before4.jpg" alt="Before 4" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before5.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before5.jpg" alt="Before 5" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\Before6.jpg">
+                                            <img src="..\..\images\Decks\Deck1\Before6.jpg" alt="Before 6" />
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <p>During:  The supports had sagged sitting on the concrete blocks.  The blocks were removed, and new supports installed fastened directly to the concrete platorm.</p>
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\During1.jpg">
+                                            <img src="..\..\images\Decks\Deck1\During1.jpg" alt="During 1" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\During2.jpg">
+                                            <img src="..\..\images\Decks\Deck1\During2.jpg" alt="During 2" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\During3.jpg">
+                                            <img src="..\..\images\Decks\Deck1\During3.jpg" alt="During 3" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\During4.jpg">
+                                            <img src="..\..\images\Decks\Deck1\During4.jpg" alt="During 4" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\During5.jpg">
+                                            <img src="..\..\images\Decks\Deck1\During5.jpg" alt="During 5" />
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <p>Finished with the railing reinstalled</p>
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After1.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After1.jpg" alt="After 1" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After2.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After2.jpg" alt="After 2" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After3.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After3.jpg" alt="After 3" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After4.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After4.jpg" alt="After 4" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After5.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After5.jpg" alt="After 5" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck1\After6.jpg">
+                                            <img src="..\..\images\Decks\Deck1\After6.jpg" alt="After 6" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container container-lg projects-grid">
+                            <div className="project">
+                                <h3>Deck Resurfacing and Leveling</h3>
+                                <p>Signifant leveling was done as some of the support posts had heaved in the ground.</p>
+                                <p>Support beams around the concrete patio had to be replaced as they were rotten.</p>
+
+                                <div className="project-grid">
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck2\After1.jpg">
+                                            <img src="..\..\images\Decks\Deck2\After1.jpg" alt="After 1" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck2\After2.jpg">
+                                            <img src="..\..\images\Decks\Deck2\After2.jpg" alt="After 2" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck2\After3.jpg">
+                                            <img src="..\..\images\Decks\Deck2\After3.jpg" alt="After 3" />
+                                        </a>
+                                    </div>
+                                    <div className="project-item">
+                                        <a href="..\..\images\Decks\Deck2\After4.jpg">
+                                            <img src="..\..\images\Decks\Deck2\After4.jpg" alt="After 4" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
