a743890d18ca95c5d359f4d9e7998c6813ab1cee
Paul Sproxton <sproxton.paul@gmail.com>
2025-07-29 18:23:10 -0400

    Trying to fix the fact that the index.html is not loading in production.

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('frontend/build'))
 
+	console.log('Index file is ', path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
 	app.get('*', (req, res) => {
-		console.log('Index file is ', path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	})
 }
