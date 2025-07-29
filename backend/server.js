e3720cdb79db1de6c1def3330780a26dcce15287
Paul Sproxton <sproxton.paul@gmail.com>
2025-07-29 18:29:06 -0400

    Fix the path in sendFile and not just the console log.

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	console.log('Index file is ', path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
 	})
 }
 
